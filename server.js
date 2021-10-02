f3719c78f6c8071b9c85b73d414fd34b712d9e87
Ribeirorodrigo05 <Ribeiro.rodrigo05@gmail.com>
2021-10-01 21:23:36 -0300

    v1.0.1
    primeiro esboço de tela de usuário

---
+++ server.js
@@ -0,0 +1,18 @@
+//dependencies
+const path = require('path');
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+
+
+
+
+
+
+
+const PORT = process.env.PORT || 5000;
+
+
+app.listen(PORT, ()=>console.log('Server is on '+ PORT))
